d07dc32da921cebeb8d1fc9dad3e3a39dadd6f94
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-02-03 10:19:59 -0300

    [client] Add @twc plugin

--- clients/react/tailwind.config.cjs
+++ clients/react/tailwind.config.cjs
@@ -1,9 +1,21 @@
+const { twTransform } = require('vite-plugin-tclasses');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: {
+    files: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./public/**/*.html",
+    ],
+    transform: (content) => {
+      if (/`@tw{/.test(content)) {
+        return twTransform(content);
+      } else {
+        return content;
+      }
+    }
+  },
   theme: {
     extend: {},
   },
