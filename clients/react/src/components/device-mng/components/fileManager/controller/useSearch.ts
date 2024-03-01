a6da228f35e3f886aa0f559870004078085c1931
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-03-01 11:18:03 -0300

    feat(react): Fix search hook

--- clients/react/src/components/device-mng/components/fileManager/controller/useSearch.ts
+++ clients/react/src/components/device-mng/components/fileManager/controller/useSearch.ts
@@ -13,15 +13,14 @@ export default function useSearch() {
 
     const deviceStore = useRxSubscription(deviceStoreEmitter, { reRenderOnChange: true });
 
-    const { files } = deviceStore.value;
-
     function updateResult(terms: string) {
+        const { files } = deviceStore.value;
         let entries: GCodeFileEntry[];
         if (terms) {
             const termsRe = new RegExp(terms, 'gi');
             entries = files.entries.filter(i => termsRe.test(i.name));
         } else {
-            entries = [...deviceStore.value.files.entries];
+            entries = [...files.entries];
         }
         fmStoreEmitter.update(d => {
             d.filesList.searchResult = entries;
@@ -39,5 +38,5 @@ export default function useSearch() {
         updateResult(fmStoreEmitter.value.filesList.searchTerms);
 
         return () => $.unsubscribe();
-    }, []);
+    }, [deviceStore.v]);
 }
